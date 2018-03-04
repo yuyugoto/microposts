9197a0e1e25555d6c737d833ab282b133b7beaf4
EC2 Default User <ec2-user@ip-172-31-44-147.us-east-2.compute.internal>
2018-03-04 12:17:58 +0000

    user login

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+(1..10).each do |number|
+  users.create(name: 'name title ' + number.to_s, email: 'test.email ' + number.to_s, password_digest: 'password ' + number.to_s)
+end
